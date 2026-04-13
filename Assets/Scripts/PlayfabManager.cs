0d8ab5ddf86270827b16800850ae8a98c8808fff
hieuvu153 <vuvuhieu153@gmail.com>
2026-04-13 23:59:29 +0700

    đky dăng nhập

--- Assets/Scripts/PlayfabManager.cs
+++ Assets/Scripts/PlayfabManager.cs
@@ -1,12 +1,44 @@
+using PlayFab;
+using PlayFab.ClientModels;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using PlayFab;
-using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayfabManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("UI")]
+    public TextMeshProUGUI messageText;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+
+    public void RegisterButton()
+    {
+        var request = new RegisterPlayFabUserRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+    }
+
+    void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        messageText.text = "Đăng ký và đăng nhập thành công!";
+    }
+    public void LoginButton()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInput.text,
+            Password = passwordInput.text
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+    }
+
     void Start()
     {
         Login();
@@ -23,14 +55,21 @@ public class PlayfabManager : MonoBehaviour
         PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
     }
 
+    void OnLoginSuccess(LoginResult result)
+    {
+        messageText.text = "Logged in!";
+        Debug.Log("Successful login/account create!");
+    }
+
     void OnSuccess(LoginResult result)
     {
-        Debug.Log("Successful login/account create!");
+        Debug.Log("Đăng nhập / tạo tài khoản thành công!");
     }
 
     void OnError(PlayFabError error)
     {
-        Debug.Log("Error while logging in/creating account!");
+        messageText.text = error.ErrorMessage;
         Debug.Log(error.GenerateErrorReport());
     }
+
 }
