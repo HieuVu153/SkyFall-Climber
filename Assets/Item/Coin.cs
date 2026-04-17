9dce8f121305af6dac1d5529cc3afccc60d78870
hieuvu153 <vuvuhieu153@gmail.com>
2026-04-17 18:02:19 +0700

    coin

--- Assets/Item/Coin.cs
+++ Assets/Item/Coin.cs
@@ -3,22 +3,9 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int value = 1;
-    public string coinID; // ID riêng cho từng coin
 
-    private void Start()
+    public int GetValue()
     {
-        // nếu coin đã ăn rồi thì xoá khi load game
-        if (PlayerPrefs.GetInt(coinID, 0) == 1)
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    public void Collect()
-    {
-        PlayerPrefs.SetInt(coinID, 1); // đánh dấu đã ăn
-        PlayerPrefs.Save();
-
-        Destroy(gameObject);
+        return value;
     }
 }
